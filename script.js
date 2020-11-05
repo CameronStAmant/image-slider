0013843a86794bdfa4e5969ba1faf0c0b3bf0b69
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-05 10:23:43 -0700

    finish step 5

--- script.js
+++ script.js
@@ -31,15 +31,26 @@ previousButton.addEventListener('click', () => {
 const dotNavigations = Array.from(document.getElementsByClassName('dot'));
 dotNavigations.forEach((item) => {
   item.addEventListener('click', () => {
+    dotNavigations[0].style['background-color'] = '#bbb';
+    dotNavigations[1].style['background-color'] = '#bbb';
+    dotNavigations[2].style['background-color'] = '#bbb';
+    dotNavigations[0].style.opacity = '0.5';
+    dotNavigations[1].style.opacity = '0.5';
+    dotNavigations[2].style.opacity = '0.5';
+    item.style['background-color'] = 'white';
+    item.style.opacity = '1.0';
     switch (item.id) {
       case '1':
         frame.style.right = '0px';
+        i = 0;
         break;
       case '2':
         frame.style.right = '200px';
+        i = 200;
         break;
       case '3':
         frame.style.right = '400px';
+        i = 400;
     }
   });
 });
