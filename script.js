8f4c19c365c82f6bab0fb002a28ef3e3a914013c
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-05 08:42:25 -0700

    buttons work

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const nextButton = document.getElementById('nextImage');
+const frame = document.getElementById('frame');
+let i = 0;
 nextButton.addEventListener('click', () => {
-  const next = document.getElementById('frame');
-  // if (next.style.right === '0px') {
-  next.classList.add('slidein');
-  // next.style.right = '200px';
-  // }
+  if (i === 0) {
+    frame.style.right = '200px';
+    i = 200;
+  } else if (i === 200) {
+    frame.style.right = '400px';
+    i = 400;
+  } else if (i === 400) {
+    frame.style.right = '0px';
+    i = 0;
+  }
+});
+
+const previousButton = document.getElementById('previousImage');
+previousButton.addEventListener('click', () => {
+  if (i === 0) {
+    frame.style.right = '400px';
+    i = 400;
+  } else if (i === 200) {
+    frame.style.right = '0px';
+    i = 0;
+  } else if (i === 400) {
+    frame.style.right = '200px';
+    i = 200;
+  }
 });
 
 nextButton;
+previousButton;
