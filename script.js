c5e9578f61dc90c5a01fafddc37a991df2e3f7b5
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-05 08:30:36 -0700

    sort of finish step 6

---
+++ script.js
@@ -0,0 +1,10 @@
+const nextButton = document.getElementById('nextImage');
+nextButton.addEventListener('click', () => {
+  const next = document.getElementById('frame');
+  // if (next.style.right === '0px') {
+  next.classList.add('slidein');
+  // next.style.right = '200px';
+  // }
+});
+
+nextButton;
