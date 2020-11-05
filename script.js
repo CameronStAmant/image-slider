0e1c9db7edb2867c8676a23fba81ee5e8d5bc4fa
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-05 12:26:48 -0700

    finish step 6 refactor

--- script.js
+++ script.js
@@ -1,60 +1,97 @@
 const nextButton = document.getElementById('nextImage');
 const frame = document.getElementById('frame');
 let i = 0;
+
 nextButton.addEventListener('click', () => {
+  dotNavCleaner();
   if (i === 0) {
-    frame.style.right = '200px';
-    i = 200;
+    setFrame2();
   } else if (i === 200) {
-    frame.style.right = '400px';
-    i = 400;
+    setFrame3();
   } else if (i === 400) {
-    frame.style.right = '0px';
-    i = 0;
+    setFrame1();
   }
 });
 
 const previousButton = document.getElementById('previousImage');
 previousButton.addEventListener('click', () => {
+  dotNavCleaner();
   if (i === 0) {
-    frame.style.right = '400px';
-    i = 400;
+    setFrame3();
   } else if (i === 200) {
-    frame.style.right = '0px';
-    i = 0;
+    setFrame1();
   } else if (i === 400) {
-    frame.style.right = '200px';
-    i = 200;
+    setFrame2();
   }
 });
 
 const dotNavigations = Array.from(document.getElementsByClassName('dot'));
 dotNavigations.forEach((item) => {
   item.addEventListener('click', () => {
-    dotNavigations[0].style['background-color'] = '#bbb';
-    dotNavigations[1].style['background-color'] = '#bbb';
-    dotNavigations[2].style['background-color'] = '#bbb';
-    dotNavigations[0].style.opacity = '0.5';
-    dotNavigations[1].style.opacity = '0.5';
-    dotNavigations[2].style.opacity = '0.5';
-    item.style['background-color'] = 'white';
-    item.style.opacity = '1.0';
+    dotNavCleaner();
     switch (item.id) {
       case '1':
-        frame.style.right = '0px';
-        i = 0;
+        setFrame1();
         break;
       case '2':
-        frame.style.right = '200px';
-        i = 200;
+        setFrame2();
         break;
       case '3':
-        frame.style.right = '400px';
-        i = 400;
+        setFrame3();
     }
   });
 });
 
+const timeout = function timer() {
+  dotNavCleaner();
+  if (i === 0) {
+    setFrame2();
+  } else if (i === 200) {
+    setFrame3();
+  } else if (i === 400) {
+    setFrame1();
+  }
+};
+
+const dotNavCleaner = function clean() {
+  dotNavigations[0].style['background-color'] = '#bbb';
+  dotNavigations[1].style['background-color'] = '#bbb';
+  dotNavigations[2].style['background-color'] = '#bbb';
+  dotNavigations[0].style.opacity = '0.5';
+  dotNavigations[1].style.opacity = '0.5';
+  dotNavigations[2].style.opacity = '0.5';
+};
+
+const setFrame1 = function frame1() {
+  frame.style.right = '0px';
+  i = 0;
+  document.getElementById('1').style['background-color'] = 'white';
+  document.getElementById('1').style.opacity = '1.0';
+};
+
+const setFrame2 = function frame2() {
+  frame.style.right = '200px';
+  i = 200;
+  document.getElementById('2').style['background-color'] = 'white';
+  document.getElementById('2').style.opacity = '1.0';
+};
+
+const setFrame3 = function frame3() {
+  frame.style.right = '400px';
+  i = 400;
+  document.getElementById('3').style['background-color'] = 'white';
+  document.getElementById('3').style.opacity = '1.0';
+};
+
+const timeoutLooper = function timer() {
+  setTimeout(function () {
+    timeout();
+    timeoutLooper();
+  }, 5000);
+};
+
 nextButton;
 previousButton;
 dotNavigations;
+setFrame1();
+timeoutLooper();
